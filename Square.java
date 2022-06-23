eb224c6430630100b199753df3205db6179b9385
PRASEN JEET <theprasenjeet@gmail.com>
2022-06-23 11:03:01 +0530

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,20 @@
+// Author : @theprasenjeet
+
+import java.util.*;
+class Square {
+public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter N : ");
+int n=sc.nextInt(); 
+System.out.print("Enter Symbol to print: ");
+char c = sc.next().charAt(0);
+for(int i=1;i<=n;i++)
+{
+for(int j=1;j<=n;j++)
+{
+System.out.print(c+" ");
+    }
+    System.out.println();
+    }
+ }
+}
